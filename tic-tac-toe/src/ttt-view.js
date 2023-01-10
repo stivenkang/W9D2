ff5d503b911fa1d4d0ae37a31342ffe8160e0bdc
Stiven Kang <kdaegyung@gmail.com>
2023-01-10 17:58:25 -0500

    setupBoard

--- tic-tac-toe/src/ttt-view.js
+++ tic-tac-toe/src/ttt-view.js
@@ -1,7 +1,23 @@
 class View {
-  constructor(game, el) {}
+  constructor(game, el) {
+    this.game = game;
+    this.el = el;
+    this.setupBoard()
+    this.bindEvents()
+    this.handleClick = this.handleClick.bind(this)
+  }
 
-  setupBoard() {}
+  setupBoard() {
+    const ul = document.createElement("ul");
+    for (let row = 0; row < 3; row++) {
+      for (let col = 0; col < 3; col++) {
+        const li = document.createElement("li");
+        li.dataset.pos = JSON.stringify([row, col])
+        ul.append(li);
+      }
+    }
+    this.el.append(ul)
+  }
   
   bindEvents() {}
 
