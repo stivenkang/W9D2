3b1c0d6a30d8a03df6dabab55455b78c5cd1a266
meihuang <meihuang8328@gmail.com>
2023-01-10 17:40:58 -0500

    tic-tac-toe

--- tic-tac-toe/src/index.js
+++ tic-tac-toe/src/index.js
@@ -1,6 +1,9 @@
-const View = // require appropriate file
-const Game = // require appropriate file
+const View = require("./ttt-view") // require appropriate file
+const Game = require("ttt_node/game.js") // require appropriate file
 
 document.addEventListener("DOMContentLoaded", () => {
   // Your code here
+  const game = new Game();
+  const rootEl = document.querySelector(".ttt");
+  new View(game, rootEl);
 });
